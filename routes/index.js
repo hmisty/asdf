13d1434066d9bd68a424542d130e0ed185402183
Evan Liu <hmisty@gmail.com>
2017-07-31 22:17:46 +0800

    fix text

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   var full_url = req.protocol + '://' + req.get('host') + req.originalUrl;
   console.log(full_url);
   wx.get_signed_data(full_url, (data) => {
-    data.title = '幻听段子酱';
+    data.title = '幻听酱';
     res.render('index', data);
   });
 });
