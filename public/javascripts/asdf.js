d94c7588279b877faa3c0c66834c1adf98d913e5
Evan Liu <hmisty@gmail.com>
2017-08-01 09:24:07 +0800

    up and down to/from server

--- public/javascripts/asdf.js
+++ public/javascripts/asdf.js
@@ -1,7 +1,8 @@
+var $$ = (x) => document.getElementById(x);
 var asdf = {};
 
 function set_control(mode, status) {
-  var control = document.getElementById("control");
+  var control = $$("control");
   if (mode == 'play') { //播放
     if (status == 'stopped') {
       delete asdf['playing'];
@@ -17,6 +18,8 @@ function set_control(mode, status) {
       delete asdf['recording'];
       control.src = "/images/stopbig@3x.png";
       control.onclick = playOrPause; //outbound to playOrPause
+      $$("subtitle").style.display = 'none';
+      $$("share").style.display = 'inline-block';
     } else if (status == 'recording') {
       asdf.recording = true;
       control.src = "/images/pausebig@3x.png";
@@ -26,16 +29,16 @@ function set_control(mode, status) {
 }
 
 function update_local_id(local_id) {
-  document.getElementById('local_id').innerHTML = local_id;
+  $$('local_id').innerHTML = local_id;
 }
 
 function update_server_id(server_id) {
-  document.getElementById('server_id').innerHTML = server_id;
+  $$('server_id').innerHTML = server_id;
 }
 
 wx.ready(function(){
   //alert('wx.ready()');
-  //document.getElementById('voice').style = 'display:block;';
+  //document.getElementById('voice').style.display = 'block;';
 
   asdf.start_record = function () {
     wx.startRecord();
@@ -88,7 +91,7 @@ wx.ready(function(){
     }
   })
 
-  asdf.upload_voice = function () {
+  asdf.upload_voice = function (next) {
     if (asdf.local_id) {
       wx.uploadVoice({
         localId: asdf.local_id, // 需要上传的音频的本地ID，由stopRecord接口获得
@@ -97,6 +100,7 @@ wx.ready(function(){
           asdf.server_id = res.serverId; // 返回音频的服务器端ID
           update_server_id(asdf.server_id);
           console.log('voice uploaded, server id is ' + asdf.server_id);
+          next();
         }
       });
     } else {
@@ -104,7 +108,7 @@ wx.ready(function(){
     }
   }
 
-  asdf.download_voice = function () {
+  asdf.download_voice = function (next) {
     if (asdf.server_id) {
       wx.downloadVoice({
         serverId: asdf.server_id, // 需要下载的音频的服务器端ID，由uploadVoice接口获得
@@ -113,13 +117,13 @@ wx.ready(function(){
           asdf.local_id = res.localId; // 返回音频的本地ID
           update_local_id(asdf.local_id);
           console.log('voice downloaded, local id is ' + asdf.local_id);
+          next();
         }
       });
     } else {
       console.log('unknown server id of voice to download');
     }
   }
-
 
 });
 
@@ -133,7 +137,15 @@ function playOrPause() {
       set_control('play', 'playing');
     }
   } else {
-    alert("没有什么可播放的，快点按钮来录一段分享给朋友们吧！");
+    if (asdf.server_id && asdf.subtitle) {
+      $$("subtitle").innerHTML = '下载中...';
+      asdf.download_voice(function () {
+        $$("subtitle").innerHTML = asdf.subtitle;
+      });
+      playOrPause();
+    } else {
+      alert("没有什么可播放的，快点按钮来录一段分享给朋友们吧！");
+    }
   }
 }
 
@@ -150,5 +162,15 @@ function recordOrStop() {
 function newRecord() {
   asdf.start_record();
   set_control('record', 'recording');
+}
+
+function share() {
+  asdf.subtitle = prompt('给你的幻听酱起个好听的标题吧？');
+  $$("subtitle").innerHTML = asdf.subtitle;
+  $$("share").style.display = 'none';
+  asdf.upload_voice(function () {
+    location.href = location.protocol + '//' + location.host + '?'
+      + 'subtitle=' + asdf.subtitle + '&sid=' + asdf.server_id;
+  });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var full_url = req.protocol + '://' + req.get('host') + req.originalUrl;
   console.log(full_url);
+  var subtitle = req.query.subtitle;
+  var server_id = req.query.sid;
   wx.get_signed_data(full_url, (data) => {
     data.title = '幻听酱';
+    data.subtitle = subtitle ? subtitle : '没有声音';
+    data.server_id = server_id ? server_id : undefined;
     res.render('index', data);
   });
 });
